529d85cadf6843de91162f5423d437e1656ae323
Niklas <nb@niklasbarning.de>
2025-11-17 21:32:59 +0100

    Revamp hero teaser visuals

--- build/hero-teaser/index.asset.php
+++ build/hero-teaser/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '753d3bff5a5c6cbf7897');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '797c96216d0913ea2125');
