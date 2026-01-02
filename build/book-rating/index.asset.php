70c99c930a624d551e0fc7546ee0bc37d4a64cad
Niklas <nb@niklasbarning.de>
2026-01-02 16:50:40 +0100

    Update styles and asset version for book rating component

--- build/book-rating/index.asset.php
+++ build/book-rating/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c999e4828fc1a5b9f36');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '08f725186faa88c52fa2');
